54e4a746914144092da804dcd8de1d83f8655024
Victor Cardona <victorhugocardonagomez@gmail.com>
2019-07-26 14:15:15 -0500

    Add SubClass Sandbox Pattern Scripts

---
+++ Assets/Scripts/Subclass Sandbox Pattern/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SubclassSandbox
+{
+    public class GameController : MonoBehaviour
+    {
+        //A list that will store all superpowers
+        List<Superpower> superPowers = new List<Superpower>();
+
+        void Start()
+        {
+            superPowers.Add(new SkyLaunch());
+            superPowers.Add(new GroundDive());
+        }
+
+        void Update()
+        {
+            //Activate each superpower each update
+            for (int i = 0; i < superPowers.Count; i++)
+            {
+                superPowers[i].Activate();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Subclass Sandbox Pattern/Superpower.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SubclassSandbox
+{
+    //This is the base class
+    public abstract class Superpower
+    {
+        //This is the sandbox method that a subclass has to have its own version of
+        public abstract void Activate();
+
+        //All of the operations a derived class needs to perform - called from Activate()
+        protected void Move(float speed)
+        {
+            Debug.Log("Moving with speed " + speed);
+        }
+
+        protected void PlaySound(string coolSound)
+        {
+            Debug.Log("Playing sound " + coolSound);
+        }
+
+        protected void SpawnParticles()
+        {
+
+        }
+    }
+
+
+    //Subclasses
+    public class SkyLaunch : Superpower
+    {
+        //Has to have its own version of Activate()
+        public override void Activate()
+        {
+            //Add operations this class has to perform
+            Move(10f);
+            PlaySound("SkyLaunch");
+            SpawnParticles();
+        }
+    }
+
+    public class GroundDive : Superpower
+    {
+        //Has to have its own version of Activate()
+        public override void Activate()
+        {
+            //Add operations this class has to perform
+            Move(15f);
+            PlaySound("GroundDive");
+            SpawnParticles();
+        }
+    }
+}
